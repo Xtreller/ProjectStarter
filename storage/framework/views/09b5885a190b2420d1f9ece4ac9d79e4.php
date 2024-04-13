c7555789b01de56ca933b014bfbcbdfc340322b4
Xtrell <vladsito@gmail.com>
2024-04-13 12:29:20 +0300

    Initial setups

--- storage/framework/views/09b5885a190b2420d1f9ece4ac9d79e4.php
+++ storage/framework/views/09b5885a190b2420d1f9ece4ac9d79e4.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -16,23 +16,25 @@
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"
             integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
 </head>
 
 <body class="font-inter bg-gray-50 dark:bg-gray-800">
 
-    @include('layouts.navigation')
+    <?php echo $__env->make('layouts.navigation', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
     <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
-        @include('layouts.aside')
+        <?php echo $__env->make('layouts.aside', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         <!-- Page Content -->
         <div id="main-content"
              class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
             <main>
-                {{ $slot }}
+                <?php echo e($slot); ?>
+
             </main>
         </div>
     </div>
 </body>
 
 </html>
+<?php /**PATH D:\Development\Work\viteDemo\resources\views/layouts/app.blade.php ENDPATH**/ ?>
--- storage/framework/views/c4fe417a391ee54938cca8c7996d899a.php
+++ storage/framework/views/c4fe417a391ee54938cca8c7996d899a.php
@@ -18,47 +18,20 @@
                     </svg>
                 </button>
                 <a href="" class="flex ml-2 md:mr-24">
-                    <x-application-logo/>
+                    <x-application-logo/x-logo-icon>
                     <span
                           class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"></span>
                 </a>
-                {{-- <form action="#" method="GET" class="hidden lg:block lg:pl-3.5">
-                    <label for="topbar-search" class="sr-only">Search</label>
-                    <div class="relative mt-1 lg:w-96">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                      clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                        <input type="text" name="email" id="topbar-search"
-                               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                               placeholder="Search">
-                    </div>
-                </form> --}}
+                
             </div>
             <div class="flex items-center">
-                {{-- <a href="{{ route('settings') }}" data-tooltip-target="tooltip-settings"
-                   class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                              clip-rule="evenodd"></path>
-                    </svg>
-                </a> --}}
+                
                 <div id="tooltip-settings" role="tooltip"
                      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                     Настройки
                     <div class="tooltip-arrow" data-popper-arrow></div>
                 </div>
-                {{-- <div class="hidden mr-3 -mb-1 sm:block">
-                    <a class="github-button" href="https://github.com/themesberg/flowbite-admin-dashboard"
-                       data-color-scheme="no-preference: dark; light: light; dark: light;" data-icon="octicon-star"
-                       data-size="large" data-show-count="true"
-                       aria-label="Star themesberg/flowbite-admin-dashboard on GitHub">Star</a>
-                </div> --}}
+                
                 <!-- Search mobile -->
                 <button id="toggleSidebarMobileSearch" type="button"
                         class="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
@@ -405,3 +378,4 @@
         </div>
     </div>
 </nav>
+<?php /**PATH D:\Development\Work\viteDemo\resources\views/layouts/navigation.blade.php ENDPATH**/ ?>
